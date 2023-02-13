621cf453934ba9502846becccca23f3202279e39
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2023-02-13 12:39:01 +0530

    stage-1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+# Button to run the Error-Level Analysis algorithm
+ela = ttk.Button(text="Error-Level Analysis", style="my.TButton", command=ela_analysis)
+ela.grid(row=6, column=0, columnspan=2, pady=5)
